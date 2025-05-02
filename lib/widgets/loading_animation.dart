a507d3cddd664079b9918585f9edb3676908946b
Amir-Ashraf <154739352+Amir-Asharf@users.noreply.github.com>
2025-05-02 22:23:58 +0300

    Add files via upload

---
+++ lib/widgets/loading_animation.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LoadingAnimation extends StatefulWidget {
+  const LoadingAnimation({Key? key}) : super(key: key);
+
+  @override
+  State<LoadingAnimation> createState() => _LoadingAnimationState();
+}
+
+class _LoadingAnimationState extends State<LoadingAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat();
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RotationTransition(
+            turns: _animation,
+            child: Icon(
+              Icons.wb_sunny_outlined,
+              size: 64,
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Loading weather data...',
+            style: Theme.of(context).textTheme.bodyLarge,
+          ),
+        ],
+      ),
+    );
+  }
+}
